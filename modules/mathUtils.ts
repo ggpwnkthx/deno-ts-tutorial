31cdbc2a8a5b56cc471472c9e2ade7272a53b964
Isaac Jessup <ibjessup@gmail.com>
2024-12-03 17:11:32 +0000

    modules and built-ins

---
+++ modules/mathUtils.ts
@@ -0,0 +1,10 @@
+export const pi = 3.14159;
+export function square(x: number): number {
+  return x * x;
+}
+export function area(radius: number): number {
+  return Math.PI * radius * radius;
+}
+export function circumference(radius: number): number {
+  return 2 * Math.PI * radius;
+}
